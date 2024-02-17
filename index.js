ff38978de29af51face48eb68fc7e356fb99e20b
darttz <darttz420@gmail.com>
2024-02-16 19:20:24 -0700

    added a 3rd route

--- index.js
+++ index.js
@@ -1,32 +1,64 @@
-const http = require('node:http');
-// console.log('What is http?', http)
-let goatCount = 0;
+const http = require("node:http");
+let hitCount = 0; //let is a way to make a mutable variable. basically saying i'm letting this be thing 'a' until i want it to change to thing 'b'.
 let errorCount = 0;
-// Create a local server to receive data from
-const handleIncomingRequest = (request, response) => {
-  console.log('What is Request URL?', request.url)
-  if (request.url === "/") {
-    goatCount += 1;
-    response.writeHead(200, {
-      'Content-Type': 'application/json',
-      'goat-integrity': 'Yes, these goats are trustworthy!',
-      'all-the-things': 'These are the things'
-    });
-    response.end(JSON.stringify({
-      data: `${goatCount} goats have visited this page!`,
-    }));
-  } else {
-    errorCount += 1;
-    response.writeHead(404, {
-      'Content-Type': 'application/json',
-      'goat-integrity': 'No, these goats are NOT trustworthy.',
-      'all-the-things': 'These are NOT the things'
-    });
-    response.end(JSON.stringify({
-      data: `${errorCount} goats are invalid!`,
-    }));
-  }
-}
-const server = http.createServer(handleIncomingRequest);
 
-server.listen(8000);
+const handleIncomingRequest = (request, response) => {
+  console.log("what is request.url?", request.url);
+  if (request.url === "/") {
+    hitCount++;
+    response.writeHead(200, {
+      "Content-Type": "text/html",
+      "racoon-invasion-status": "the racoons have not yet invaded. we're safe.... for now."
+    });
+    response.end(`
+    <!doctype html />
+    <html>
+      <head>
+        <title> welcome to api </title>
+      </head>
+      <body>
+        <h1> holy tamales batman!! </h1>
+        <p> there are tamales everywhere.</p>
+        <p> ${hitCount} tamales have been made here...</p>
+      </body>
+    </html>
+    `);
+    console.log("what is hit count?", hitCount);
+  } else if(request.url === "/frogs"){
+    hitCount++;
+    response.writeHead(418, {
+      "Content-Type": "application/json",
+      "racoon-invasion-status": "the frogs came over for tea."
+    });
+    response.end(
+      JSON.stringify({
+        data: hitCount + " goats have hopped around this pond!",
+      })
+    );
+    console.log("what is ribbit count?", hitCount);
+
+  } else {
+    errorCount++;
+    response.writeHead(404, {
+      "Content-Type": "text/html",
+      "racoon-invasion-status": "THE RACOONS ARE INVADING!! WE'RE ALL DOOOOMED!!"
+    });
+    response.end(`
+    <!doctype html />
+    <html>
+      <head>
+        <title> welcome to api </title>
+      </head>
+      <body>
+        <h1> Sorry batman (wah wah wahhhh) </h1>
+        <p> only robin is here, and he ate all the tamales.</p>
+        <p> ${errorCount} tamales have been made here...</p>
+      </body>
+    </html>
+    `);
+  }
+};
+// Create a local server to receive data from
+const server = http.createServer(handleIncomingRequest);
+// tell the server to listen for incoming requests on port 1400
+server.listen(1400);
